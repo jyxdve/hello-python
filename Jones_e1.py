acce0c00b237deeea93d10b8e239e621f2e31b34
jyxdve <jyxdvejns@gmail.com>
2018-10-25 23:16:43 +0800

    High

---
+++ Jones_e1.py
@@ -0,0 +1,16 @@
+data ={"Name" : "Jayx Dave Jones",
+       "Spirit Animal" : "Dog",
+       "Reason" : "of its aura",
+       "Hobby" : "sing and sleep",
+       "Profession" : "Civil Engineering"
+       }
+
+print("I am {}.".format(data["Name"]))
+print("My spirit animal is {}.".format(data["Spirit Animal"]))
+print("because {}.".format(data["Reason"]))
+print("When not in school, I love to {}.".format(data["Hobby"]))
+print("I dream to become a/an {} in the future.".format(data["Profession"]))
+
+
+
+
--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print("Hello, World!")
-print("My name is {}. I am {} y/o." .format("Dave", 17))
+print("My name is {}. I am {} y/o.". format("Dave", 17))
+print("AdNU",2018, sep="-", end="")
+print("CSNHS")
