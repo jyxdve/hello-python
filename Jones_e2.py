051e0522952d6beff4f0055f0829e86ed58cd16b
jyxdve <jyxdvejns@gmail.com>
2018-10-26 16:48:17 +0800

    ey

--- Jones_e2.py
+++ Jones_e2.py
@@ -1,15 +1,14 @@
 import sys
 
-WindIntensity=(sys.argv[1])
-WindIntensity = float(WindIntensity)
+WindIntensity= float(sys.argv[1])
 
-if  WindIntensity >=220.0:
+if  WindIntensity >= 220:
     print("Super Typhoon")
-elif WindIntensity >=118.o:
+elif WindIntensity >= 118 or  WindIntensity == 219.99:
     print("Typhoon")
-elif WindIntensity >=89.0:
+elif WindIntensity >= 89 or   WindIntensity == 117.99:
     print("Severe Tropical Strorm")
-elif WindIntensity >=62.0:
+elif WindIntensity >= 62 or   WindIntensity == 88.99:
     print("Tropical Storm")
 else:
     print("Tropical Depression")
--- Jones_e3.py
+++ Jones_e3.py
@@ -1,11 +1,15 @@
 input = input("Enter a comma separated list of numbers:")
+user_input = number_input
 
-x = 1
-while x<1:
-    if x& 1:
-        print(x, end = "")
-        total += x
-    x += 1
+for x in user_input:
+    sum += float(user_input[index])
+    user_input = user_input("Sum of the number")
+
+#while x<1:
+    #if x& 1:
+       # print("Sum of Squares")
+        #total += x
+    #x += 1
     #print("Sum of Squares:")
 #input = input ("Sum of Squares:")
 
