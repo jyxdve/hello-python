85e7a4a1fc4418f75de4d93e199a03bf750e8c7d
jyxdve <jyxdvejns@gmail.com>
2018-10-26 17:03:28 +0800

    High

--- Jones_e2.py
+++ Jones_e2.py
@@ -4,11 +4,11 @@ windintensity = float(sys.argv[1])
 
 if  windintensity >= 220:
     print("Super Typhoon")
-elif windintensity >= 118 or windintensity == 219.99:
+elif windintensity >= 118 or windintensity == 219:
     print("Typhoon")
-elif windintensity >= 89 or windintensity == 117.99:
-    print("Severe Tropical Strorm")
-elif windintensity >= 62 or windintensity == 88.99:
+elif windintensity >= 89 or windintensity == 117:
+    print("Severe Tropical Storm")
+elif windintensity >= 62 or windintensity == 88:
     print("Tropical Storm")
 else:
     print("Tropical Depression")
